184554228e8907966e27ea57c34b57714375a302
Faysal2123 <foysal2005n1@gmail.com>
2025-03-24 00:22:58 +0600

    Implement SignUp form UI with basic form handling

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from '../components/Shared/NavBar';
 import Footer from '../components/Shared/Footer';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
         <div>
-          <NavBar></NavBar>  
+          <NavBar></NavBar>
+          <div><Outlet></Outlet></div>  
           <Footer></Footer>
         </div>
     );
--- src/components/Shared/NavBar.jsx
+++ src/components/Shared/NavBar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavBar = () => {
+    const [isOpen,setIsOpen]=useState(false)
   return (
     <div>
       <div className="navbar bg-base-100 shadow-sm">
@@ -17,19 +18,21 @@ const NavBar = () => {
              
             </div>
             <div
+           
               tabIndex={0}
               className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
             >
              
             </div>
           </div>
-          <div className="dropdown dropdown-end">
+          <div  className="dropdown dropdown-end">
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
-              <div className="w-10 rounded-full">
+              <div  className="w-10 rounded-full">
+                
                 <img
                   alt="Tailwind CSS Navbar component"
                   src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
@@ -37,19 +40,15 @@ const NavBar = () => {
               </div>
             </div>
             <ul
+               
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
+            
+              <li onClick={()=>setIsOpen(!open)}>
                 <a>Settings</a>
               </li>
-              <li>
+              <li onClick={()=>setIsOpen(!open)}>
                 <a>Logout</a>
               </li>
             </ul>
